3f7f0c2c56e9bd7e16a09291d5757484c1362fb4
LocKey <rudfhr97@naver.com>
2022-12-28 14:06:36 +0900

    feat: 채팅목록화면 구현시작

---
+++ lib/chatList_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatList extends StatelessWidget {
+  const ChatList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
