b5b1638e0425c7daec216a8fb64a71367ed9487d
k-hyeongi <21900215@handong.ac.kr>
2022-12-30 16:44:52 +0900

    feat : LoginPage 프론트엔드 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:han_bab/screens/login/login_page.dart';
 import 'package:han_bab/screens/main/main_screen.dart';
 import 'firebase_options.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainScreen(),
+      home: LoginPage(),
     );
   }
 }
