3c4a3b00c187fd12772171c33812f0744ba3b993
hgkim00 <21900215@handong.ac.kr>
2023-01-16 15:01:52 +0900

    feat : splash screen & app logo (#34)

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,82 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart'; //안드로이드 디자인
 import 'package:flutter/cupertino.dart'; //IOS 디자인홈 화면 함수 불러오기 불러오기
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:han_bab/view/loading/splash_screen.dart';
+import 'package:get/get.dart';
+import 'package:han_bab/controller/auth_controller.dart';
 import 'package:han_bab/view/login/login_page.dart';
-import 'package:han_bab/view/login/verify_login_page.dart';
-import 'package:han_bab/view/main/main_screen.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   KakaoSdk.init(nativeAppKey: '7a0fe1780f619b2dccaea7d4ddbaea70');
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print("initialized...");
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: Future.delayed(Duration(seconds: 2), () => 100),
-        builder: (context, snapshot) {
-          return MaterialApp(
-            home: AnimatedSwitcher(
-              duration: Duration(milliseconds: 900),
-              child: _splashLoadingWidget(snapshot),
-            ),
-          );
-        });
-  }
-}
-
-StatefulWidget _splashLoadingWidget(AsyncSnapshot<Object?> snapshot) {
-  final _authentication = FirebaseAuth.instance;
-
-  if (snapshot.hasError) {
-    debugPrint("에러 발생");
-    return SplashScreen();
-  } else if (snapshot.connectionState == ConnectionState.done) {
-    return StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          if (FirebaseAuth.instance.currentUser!.emailVerified) {
-            final userName = _authentication.currentUser!.displayName;
-            showToast(userName);
-            return MainScreen();
-          } else {
-            return VerifyLoginPage();
-          }
-        } else {
-          return LoginPage();
-        }
-      },
+    return GetMaterialApp(
+      home: LoginPage(),
     );
-  } else {
-    return SplashScreen();
+    // });
   }
 }
+
+// StatefulWidget _splashLoadingWidget(AsyncSnapshot<Object?> snapshot) {
+//   // final _authentication = FirebaseAuth.instance;
+//
+//   if (snapshot.hasError) {
+//     debugPrint("에러 발생");
+//     return SplashScreen();
+//   } else if (snapshot.connectionState == ConnectionState.done) {
+//     return LoginPage();
+//     // return StreamBuilder(
+//     //   stream: FirebaseAuth.instance.authStateChanges(),
+//     //   builder: (context, snapshot) {
+//     //     if (snapshot.hasData) {
+//     //       if (FirebaseAuth.instance.currentUser!.emailVerified) {
+//     //         final userName = _authentication.currentUser!.displayName;
+//     //         showToast(userName);
+//     //         return MainScreen();
+//     //       } else {
+//     //         return VerifyLoginPage();
+//     //       }
+//     //     } else {
+//     //       return LoginPage();
+//     //     }
+//     //   },
+//     // );
+//   } else {
+//     return SplashScreen();
+//   }
+// }
 
 void showToast(String? userName) {
   Fluttertoast.showToast(
